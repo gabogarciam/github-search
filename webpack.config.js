0a5f2791941194a182dddbc35cde7849df616828
gabogarciam <gabogarciam71@gmail.com>
2020-10-09 22:43:24 +0200

    added babel and webpack server configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
-const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-// const webpackNodeExternals = require('webpack-node-externals');
 
 module.exports = {
     entry: {
@@ -14,6 +12,10 @@ module.exports = {
         filename: 'bundle.js'
     },
     target: 'node',
+    node: {
+        __dirname: false,
+        __filename: false
+    },
     externals: [nodeExternals()],
     module: {
         rules: [
