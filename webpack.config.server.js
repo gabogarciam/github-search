73a2fa34a92ae036144feaa973a8ac24edcbf7a5
gabogarciam <gabogarciam71@gmail.com>
2020-10-12 22:14:19 +0200

    fix identation

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -9,20 +9,20 @@ module.exports = {
         path: path.join(__dirname, 'dist'),
         publicPath: '/',
         filename: '[name].js'
-  },
-  target: 'node',
-  node: {
-      __dirname: false,
-      __filename: false,
-  },
-  externals: [nodeExternals()],
-  module: {
-      rules: [
-          {
-              test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
+    },
+    target: 'node',
+    node: {
+        __dirname: false,
+        __filename: false,
+    },
+    externals: [nodeExternals()],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
                 }
             }
         ]
