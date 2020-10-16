aab538c6951aba2869a377a00573c7fe20a31db8
gabogarciam <gabogarciam71@gmail.com>
2020-10-16 18:43:14 +0200

    applied the logic of the API and UI

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 import './public/stylesheets/style.scss';
 
-const UserInterface = require('./public/javascripts/message');
+const sharingMethods = require('./public/javascripts/message');
+const apiGitHub = require('./controllers/api');
+const UserInterface = require('./controllers/ui');
 
+const msg = new sharingMethods();
+const retriveUserData = new apiGitHub();
 const ui = new UserInterface();
 
-ui.logMessage('Welcome to GitHub Search! - This project is made in node with Love');
+msg.logMessage('Welcome to GitHub Search! - This project is made in node with Love');
+
+const userForm = document.getElementById('userForm');
+
+userForm.addEventListener('submit', (event) => {
+    const textSearch = userForm.querySelector('#textSearch').value;
+
+    if (textSearch != '') {
+        retriveUserData.fetchUser(textSearch)
+            .then((data) => {
+                if (data.userData.message === "Not Found") {
+                    ui.clearError();
+                    ui.showError(`We couldn’t find any user matching with '${textSearch}'`)
+                    return;
+                };
+                ui.clearError();
+                console.log('Yeee it works!!!');
+            });
+    } else {
+        ui.clearError();
+        ui.showError('Field Search is empty')
+    }
+    event.preventDefault();
+
+});
